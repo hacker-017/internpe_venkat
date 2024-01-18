ea8ef7b3a4ea1d0ce59a00113725ff26d11906f8
hacker-017 <appu171205@gmail.com>
2024-01-18 20:29:20 +0530

    added successfully

--- clock.py
+++ clock.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 
 def update_time():
     current_time = time.strftime("%I:%M:%S %p")
-    label.config(text=current_time,foreground=f"#{random.randint(0, 0xFFFFFF):06x}",background=f"#{random.randint(0, 0xFFFFFF):06x}")
+    label.config(text=current_time,foreground=f"#{random.randint(0, 0xFFFFFF):06x}")
     root.after(1000, update_time)
 
 root = tk.Tk()
